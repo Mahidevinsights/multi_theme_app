20289ab8adb12eaf62c4c17cabad75ac9a7bfe46
Mahidhar Mogili <mahidevinsights@gmail.com>
2024-11-29 14:42:58 +0530

    null check operator check

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:multi_theme_app/styles/multi_themes/blue_theme.dart';
 import 'package:multi_theme_app/styles/multi_themes/dark_theme.dart';
 import 'package:multi_theme_app/styles/multi_themes/light_theme.dart';
-import 'package:multi_theme_app/utils/enums.dart';
 import 'multi_themes/green_theme.dart';
+import 'package:multi_theme_app/utils/enums.dart';
 
 class AppColors {
   static AppThemes currentTheme = AppThemes.light;
@@ -16,18 +16,24 @@ class AppColors {
     AppThemes.green: GreenTheme.colors,
   };
 
-  // Centralized Color Access
-  static Map<String, Color> get colors => _themeMap[currentTheme]!;
+  // Centralized Color Access with Fallback
+  static Map<String, Color> get colors =>
+      _themeMap[currentTheme] ?? LightTheme.colors;
 
-  static Color get primary => colors['primary']!;
-  static Color get background => colors['background']!;
-  static Color get foreGround => colors['foreGround']!;
-  static Color get cardBorder => colors['cardBorder']!;
-  static Color get depth => colors['depth']!;
-  static Color get secondary => colors['secondary']!;
-  static Color get primaryText => colors['primaryText']!;
-  static Color get secondaryText => colors['secondaryText']!;
-  static Color get positiveIndicator => colors['positiveIndicator']!;
-  static Color get negativeIndicator => colors['negativeIndicator']!;
-  static Color get textColor => colors['textColor']!;
+  static Color _getColor(String key) {
+    // Check if the key exists in the current theme; otherwise, fallback to LightTheme
+    return colors[key] ?? LightTheme.colors[key] ?? Colors.transparent;
+  }
+
+  static Color get primary => _getColor('primary');
+  static Color get background => _getColor('background');
+  static Color get foreGround => _getColor('foreGround');
+  static Color get cardBorder => _getColor('cardBorder');
+  static Color get depth => _getColor('depth');
+  static Color get secondary => _getColor('secondary');
+  static Color get primaryText => _getColor('primaryText');
+  static Color get secondaryText => _getColor('secondaryText');
+  static Color get positiveIndicator => _getColor('positiveIndicator');
+  static Color get negativeIndicator => _getColor('negativeIndicator');
+  static Color get textColor => _getColor('textColor');
 }
